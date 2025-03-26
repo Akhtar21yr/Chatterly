6c00d603c1811f4133c23d6b8372a294a40179de
akhtar21yr <akhtar.decy@gmail.com>
2025-03-26 15:40:22 +0530

    create account service

--- chatterly/chatterly/settings.py
+++ chatterly/chatterly/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fta#l*wnd$ap7^=r8t0-!&0=23%*ipu78hog(0-@wy+1nufm+j'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
+print('DEBUG>>>>>>>>>>>>>>>>>>>>>>>>>>',DEBUG)
 
 ALLOWED_HOSTS = []
 
@@ -37,6 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'account',
+    'server'
 ]
 
 MIDDLEWARE = [
@@ -121,3 +128,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+AUTH_USER_MODEL = 'account.Account'
---
+++ chatterly/server/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.conf import settings
+
+# Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    desc = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Server(models.Model):
+    name = models.CharField(max_length=100)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
